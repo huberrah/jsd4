4919b750d199fb512ec3b29423137786cefd0bbb
huberrah <Frinkles_1>
2016-10-12 18:54:55 -0700

    Add Hw3

---
+++ Lessons/notes.js
@@ -0,0 +1,11 @@
+console.log("Elements");
+
+var brands = document.querySelector(".brands");
+
+var li = document.createElement("li");
+
+li.innerHTML = "reebok";
+
+brands.appendChild(li);
+
+google caniuse and javascript element and there's a graph of everything used
