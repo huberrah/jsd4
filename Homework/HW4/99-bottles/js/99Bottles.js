6727b0af01d0bc34b160f5f249179046ec5f3887
huberrah <Frinkles_1>
2016-10-18 22:13:01 -0700

    Add HW to index

--- Homework/HW4/99-bottles/js/99Bottles.js
+++ Homework/HW4/99-bottles/js/99Bottles.js
@@ -15,16 +15,16 @@ for(i = 99; i >= 0; i--){
 	l2 = "Take one down, and pass it around " + (i-1) +  " bottles of beer on the wall."
 	}
 
-	else if(i==2){
+	if(i==2){
 	l1 = i + " bottles of beer on the wall, " + i + " bottles of beer.</br>"
 	l2 = "Take one down and pass it around, " + (i-1) + " bottle of beer on the wall."
 	}	 
 
-	else if(i==1) {
+	if(i==1) {
 	l1 = i + "bottle of beer on the wall, " + i + " bottle of beer.<br>"
 	l2 = "Take one down and pass it around, no more bottles of beer on the wall"
 	}
-	else if(i==0){
+	if(i==0){
 	l1 = "No more bottles of beer on the wall, no more bottles of beer<br>"
 	l2 = "Go to the store and buy some more, 99 bottles of beer on the wall"
 	}
--- Lessons/score-keeper-starter/js/score-keeper.js
+++ Lessons/score-keeper-starter/js/score-keeper.js
@@ -8,12 +8,23 @@ var ssBtn = document.querySelector("#submit-custom-score");
 var plus5Btn = document.querySelector("#increase-5");
 var min5Btn = document.querySelector("#decrease-5")
 
-function fnSetScore() {
-if(setScore == "") {
+function fnSetScore(){
+var iAN = isNaN(setScore.value);
+
+if(setScore.value == "" || setScore.value == null) {
 alert("Please enter set score field");
 }
 
-else {
+else if(setScore.value == "Chuck Norris") {
+alert("The GA team knows that you don't find Chuck Norris.  Chuck Norris finds you!");
+}
+
+else if(iAN){
+alert("Please enter a NUMBER");
+}
+
+else if(iAN == false && setScore != ""){
+console.log("I am in the last else if");
 actualScore.innerHTML = setScore.value;
 } 
 }
@@ -21,7 +32,7 @@ actualScore.innerHTML = setScore.value;
 function plusFive() {
 if(actualScore != "") {
 score = parseInt(actualScore.innerHTML);
-score = score + 5;
+score = parseInt(score + 5);
 actualScore.innerHTML  = score;
 }
 }
@@ -29,11 +40,11 @@ actualScore.innerHTML  = score;
 function minusFive() {
 if(actualScore != "") {
 score = parseInt(actualScore.innerHTML);
-score = score - 5;
+score = parseInt(score) - 5;
 actualScore.innerHTML = score;
 }
 }
 //setTheListeners
-ssBtn.addEventListener("click", setScore);
+ssBtn.addEventListener("click", fnSetScore);
 plus5Btn.addEventListener("click", plusFive);
 min5Btn.addEventListener("click", minusFive);
